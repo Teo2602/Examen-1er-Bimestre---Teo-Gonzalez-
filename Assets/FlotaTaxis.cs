f88613150bf33ea270cb2c3b570893e3245b08c6
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2025-04-28 11:27:01 -0300

    declaración variables

---
+++ Assets/FlotaTaxis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlotaTaxis : MonoBehaviour
+{
+    public int cantidadTaxis;
+    public int cantidadDias;
+    int taxisKM;
+    int KMporDia = 90;
+    int KMporL = 15;
+    int precioPorL = 130;
+    int KMtotal;
+    int Ltotal;
+    int precioTotal;
+    int precioDescuento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cantidadDias < 5)
+        {
+            Debug.Log("ERROR: La cantidad de dias debe ser mayor que 5");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
